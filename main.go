3afd59cc036b80c26b5a413bda2fd6a6a08ff364
Jose Rubio <jadrubio@gmail.com>
2024-04-24 14:56:27 -0400

    deck file and print function

--- main.go
+++ main.go
@@ -1,18 +1,15 @@
 package main
 
-import "fmt"
-
 func main() {
 	suites := []string{"Hearts", "Diamonds", "Spades", "Clubs"}
 	faces := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
 
-	cards := []string{}
+	cards := deck{}
 	for _, s := range suites {
 		for _, f := range faces {
 			cards = append(cards, f+" of "+s)
 		}
 	}
-	for _, d := range cards {
-		fmt.Println(d)
-	}
+
+	cards.print()
 }
